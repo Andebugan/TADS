9ae1e8298a9161814599a97ac6205489717372b8
Rull Deef <deeroll666@gmail.com>
2021-01-09 13:46:41 +0000

    add prompt func

--- TADS_2/src/main.c
+++ TADS_2/src/main.c
@@ -7,14 +7,23 @@ int main(void)
 {
     int status = SUCCESS;
 
+    /*
     status = ctrt_load_from_file("data.txt");
     if (status == SUCCESS)
     {
-        float time = ctrt_mid_time(1000, ctrt_sort_slow_table, ctrt_shuffle_table);
-        ctrt_print();
+        // ctrt_print();
+        float time = ctrt_mid_time(100000, ctrt_sort_fast_table, ctrt_shuffle_table);
+        printf("среднее время сортировки (fast): %.2f мкс\n", time);
 
-        printf("среднее время сортировки: %.2f мкс\n", time);
+        time = ctrt_mid_time(100000, ctrt_sort_slow_table, ctrt_shuffle_table);
+        printf("среднее время сортировки (slow): %.2f мкс\n", time);
     }
+    */
+    country_t ctr;
+    status = ctr_prompt(&ctr);
+
+    if (status == SUCCESS)
+        printf("ok!\n");
 
     return status;
 }
